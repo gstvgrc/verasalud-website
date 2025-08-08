ceed3135a15f2596b5eb3d4aced4a070a11b8e47
gstvgrc <48648155+gstvgrc@users.noreply.github.com>
2025-08-08 07:47:55 -0500

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -52,12 +52,24 @@ const nextConfig = {
             value: 'nosniff'
           },
           {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
+            value: 'strict-origin-when-cross-origin'
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+            value: 'geolocation=(), microphone=(), camera=(), interest-cohort=()'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:;"
           }
         ]
       },
